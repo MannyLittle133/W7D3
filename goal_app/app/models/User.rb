fce399d00eef6079f9b70a514b8a9227840e7d6e
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-12-21 17:02:43 -0500

    cant figure out this users error

--- goal_app/app/models/User.rb
+++ goal_app/app/models/User.rb
@@ -1,7 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  session_token   :string           not null
+#
 class User < ApplicationRecord
     validates :username, presence: true
     validates :password_digest, presence: true
     validates :password, length: {minimum: 6}, allow_nil: true
+    before_validation :ensure_session_token
 
     attr_reader :password
 
@@ -10,6 +22,21 @@ class User < ApplicationRecord
         @password = password
     end
 
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
+    def reset_session_token
+        self.session_token = generate_session_token
+        self.save! 
+        self.session_token
+    end
+
+    private
+
+    def generate_session_token 
+        SecureRandom::urlsafe_base64
+    end
 
 
 end
