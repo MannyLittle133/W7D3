bb14dedaa4f4019eb80c276207b2f53d5a1bb19e
Logan Hartman <loganchart@gmail.com>
2022-12-21 16:23:43 -0500

    factory users

---
+++ goal_app/app/models/User.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+    validates :username, presence: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+
+
+end
--- goal_app/spec/models/users_spec.rb
+++ goal_app/spec/models/users_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
-RSpec.descibe User, type: :model do 
+RSpec.describe User, type: :model do 
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password_digest) }
-    it { should validate_uniqueness_of(:username) }
+    
+    it { should validate_length_of(:password).is_at_least(6) }
+    
+    # describe 'uniqueness' do
+    #     before :each do
+    #         create(:user)
+    #     end
+
+    #     it { should validate_uniqueness_of(:username) }
+    #     # it { should validate_uniqueness_of(:session_token)}
+
+    # end
+
+
 end
